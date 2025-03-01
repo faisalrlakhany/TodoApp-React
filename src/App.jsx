7d1ae5ecf445fed58e03dddae8c10b79ab8c273d
faisalrlakhany <lakhanyfaisal37@gmail.com>
2025-03-01 16:07:51 +0500

    onchange task completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {useState} from 'react'
 
-  return (
+function App() {
+  
+
+  const [todo , setTodo] = useState('')
+  const [content , setContent] = useState('')
+
+  function addTodo(event){
+
+    console.log("button click");
+    event.preventDefault();
+    console.log(todo);
+    console.log(content);
+    
+    setTodo('')
+    setContent('')
+  }
+
+  return(
+
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <h1>TODO APP</h1>
+
+      <form 
+      onSubmit={addTodo}
+      >
+
+       <input
+        type="text"
+        onChange={(e)=>{setTodo(e.target.value)}} 
+        value={todo}   
+        placeholder="Enter Todo" 
+        />
+
+        <br/>
+        <br/>
+
+        <input 
+        type="text" 
+        onChange={(e)=>{setContent(e.target.value)}} 
+        value={content} 
+        placeholder="Enter Content"  
+        />
+
+        <br/>
+        <br/>
+    
+        <button>Add</button>
+
+      </form>
     </>
+
   )
 }
 
